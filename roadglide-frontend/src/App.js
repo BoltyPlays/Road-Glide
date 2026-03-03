b3531550c20f3aeca4d13141b244c7d2ace31f51
msoloho <msoloho@gmail.com>
2026-03-03 16:59:49 -0500

    did some of app.js

--- roadglide-frontend/src/App.js
+++ roadglide-frontend/src/App.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './App.css';
+
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
