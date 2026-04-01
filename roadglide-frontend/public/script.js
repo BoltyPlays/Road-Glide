de64686142e38d113f2e48e794fe0793282ee12a
msoloho <msoloho@gmail.com>
2026-04-01 13:26:35 -0400

    fixed script.js

--- roadglide-frontend/public/script.js
+++ roadglide-frontend/public/script.js
@@ -1,9 +1,8 @@
-var map = L.map('map').setView([45.4215, -75.6972], 13)
-L.titleLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const map = L.map('map').setView([45.4215, -75.6972], 13)
+L.titelayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Parliment Hill marker test
-L.marker([45.4235, -75.7009]).addTo(map)
-    .bindPopup('RoadGlide Tracking: Parliment')
-    .openPopup();
+const testMarker = L.marker ([45.4235, -75.7009]).addTo(map);
+testMarker.bindPopup("test")
