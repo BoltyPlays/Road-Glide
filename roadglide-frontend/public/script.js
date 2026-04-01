36c9214a13041c28f9b510da2499468b7093667e
msoloho <msoloho@gmail.com>
2026-04-01 14:24:49 -0400

    67

--- roadglide-frontend/public/script.js
+++ roadglide-frontend/public/script.js
@@ -1,17 +1,12 @@
-console.log("Map script is starting...");
-
-const map = L.map('map').setView([45.4215, -75.6972], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+console.log("Map script is starting...")
+const map = L.map('map').setView([45.4215, -75.6972], 13)
+L.tilelayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; OpenStreetMap'
+    attribution: '© OpenStreetMap contributors'
 }).addTo(map);
-
 L.marker([45.4235, -75.7009]).addTo(map)
-    .bindPopup('Ottawa Test Point')
-    .openPopup();
 
 setTimeout(() => {
-   map.invalidateSize();
-   console.log("Map resize triggered");
+    map.invalidateSize();
+    console.log()
 }, 200);
