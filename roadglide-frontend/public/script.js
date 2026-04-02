83108a5a543c721f914a0368ce2ed32f82ed5a00
msoloho <msoloho@gmail.com>
2026-04-02 09:38:21 -0400

    different tiles

--- roadglide-frontend/public/script.js
+++ roadglide-frontend/public/script.js
@@ -2,13 +2,12 @@ console.log("Map script is starting...");
 
 const map = L.map('map').setView([45.4215, -75.6972], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; OpenStreetMap'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri'
 }).addTo(map);
 
 L.marker([45.4235, -75.7009]).addTo(map)
-    .bindPopup('test')
+    .bindPopup('Parliment Hill')
     .openPopup();
 
 setTimeout(() => {
