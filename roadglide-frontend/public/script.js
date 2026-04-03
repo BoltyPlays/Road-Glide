0911109e064cfda88e0eb454078a786a6fdd474f
msoloho <msoloho@gmail.com>
2026-04-03 18:48:38 -0400

    javascript done for now

--- roadglide-frontend/public/script.js
+++ roadglide-frontend/public/script.js
@@ -3,14 +3,24 @@ console.log("Map script is starting...");
 const map = L.map('map').setView([45.4215, -75.6972], 13);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',//makes everything load faster by loading from multiple servers ❤️‍🩹//
+    maxZoom: 19.67
 }).addTo(map);
 
-L.marker([45.4235, -75.7009]).addTo(map)
-    .bindPopup('Parliment Hill')
-    .openPopup();
+const geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false
+})
+.on('markgeocode', function(e) {
+    var bbox = e.geocode.bbox;
+
+    L.marker(e.geocode.centre).addTo(map)
+        .bindPopup(e.geocode.name)
+        .openPopup();
+})
+.addTo(map);
 
 setTimeout(() => {
    map.invalidateSize();
    console.log("Map resize triggered");
-}, 200);
+}, 367);
