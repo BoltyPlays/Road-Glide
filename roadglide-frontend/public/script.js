3523d5431cc9a17783c3f6ecd6149302fc3dc8e1
msoloho <msoloho@gmail.com>
2026-04-02 21:28:36 -0400

    html done for now

--- roadglide-frontend/public/script.js
+++ roadglide-frontend/public/script.js
@@ -2,8 +2,8 @@ console.log("Map script is starting...");
 
 const map = L.map('map').setView([45.4215, -75.6972], 13);
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
 L.marker([45.4235, -75.7009]).addTo(map)
